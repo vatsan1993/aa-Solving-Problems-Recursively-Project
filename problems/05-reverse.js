67cbae1ec778d105d7f89e6c58d05e824749a8e6
Srivatsan <vatsan1993@gmail.com>
2023-05-07 14:43:07 +0530

    finished 5th program

--- problems/05-reverse.js
+++ problems/05-reverse.js
@@ -13,6 +13,14 @@ reverse(""); // ""
 ***********************************************************************/
 
 // your code here
+let reverse = (string) => {
+  if(string.length === 0){
+    return string;
+  }
+  return reverse(string.slice(1)) + string[0];
+}
+
+console.log(reverse("house"));
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
