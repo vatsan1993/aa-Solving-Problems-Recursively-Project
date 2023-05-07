67a898dcbb36885484f33ccf583e547e3a0b9553
Srivatsan <vatsan1993@gmail.com>
2023-05-07 14:02:57 +0530

    finished 4th program

--- problems/04-range.js
+++ problems/04-range.js
@@ -12,7 +12,12 @@ range(7, 6); // []
 
 
 // your code here
-
+let range = (num1, num2) => {
+  if(num1 >= num2){
+    return [];
+  }
+  return [num1].concat(range(num1+1, num2))
+}
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
